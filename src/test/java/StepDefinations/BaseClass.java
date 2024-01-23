1e29ab6d74d265a0c179b4d1bcdd3e9f337539e6
SuvenduExa <155539604+SuvenduExa@users.noreply.github.com>
2024-01-23 13:11:31 +0530

    Revert "Committing"

--- src/test/java/StepDefinations/BaseClass.java
+++ src/test/java/StepDefinations/BaseClass.java
@@ -6,7 +6,6 @@ import org.apache.log4j.Logger;
 //import java.util.Properties;
 import org.openqa.selenium.WebDriver;
 
-import PageObjects.Assign_Asset_For_Hospitality;
 import PageObjects.Login;
 
 
@@ -20,8 +19,6 @@ public class BaseClass
 	
 	
 	public Login ln;
-	public Assign_Asset_For_Hospitality ah;
-//	public Assign_AssetFor_Hospitality ah;
 	
 	
 	
